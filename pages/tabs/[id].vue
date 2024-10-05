19f37060b99390ec9f21e09427cf54803c537618
Dean Gibson <dean.gibson@superbalist.com>
2024-10-05 18:22:54 +0200

    basic tab detail page

---
+++ pages/tabs/[id].vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+  const route = useRoute()
+</script>
+
+<template>
+  <div class="h-screen bg-neutral-100">
+    <Header />
+
+    <main class="container mx-auto pt-8">
+      <PageTitle :title="`Tab ${route.params.id}`">
+        <Button @click="() => console.log('new order click')">New Order</Button>
+      </PageTitle>
+
+      <div class="rounded-md bg-white p-6 shadow">
+        <span>Content</span>
+      </div>
+    </main>
+  </div>
+</template>
