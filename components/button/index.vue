57e3f2c0bd359b10331f7501b7991c40d1bd8450
Dean Gibson <dean.gibson@superbalist.com>
2024-10-07 09:15:06 +0200

    addedd button variants

--- components/button/index.vue
+++ components/button/index.vue
@@ -1,10 +1,35 @@
 <script setup lang="ts">
+  import { cva } from 'class-variance-authority'
+
+  defineProps<{
+    variant?: 'default' | 'link'
+    size?: 'default' | 'sm'
+  }>()
+
   defineEmits(['click'])
+
+  const button = cva('transition-all', {
+    variants: {
+      variant: {
+        default:
+          'rounded-md bg-green-500 px-4 py-2 hover:bg-green-600 text-white',
+        link: 'text-neutral-900 hover:text-green-600 hover:underline font-normal',
+      },
+      size: {
+        default: 'text-lg font-medium',
+        sm: 'text-base',
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
+      size: 'default',
+    },
+  })
 </script>
 
 <template>
   <button
-    class="rounded-md bg-green-500 px-4 py-2 text-lg font-medium text-white transition-all hover:bg-green-600"
+    :class="button({ size, variant })"
     @click="$emit('click')"
   >
     <slot />
