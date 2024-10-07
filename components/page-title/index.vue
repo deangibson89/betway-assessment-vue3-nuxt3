4e713797dc7de58fb0a386011e3b734d2cc66907
Dean Gibson <dean.gibson@superbalist.com>
2024-10-07 10:11:46 +0200

    added subtitle to page-title component

--- components/page-title/index.vue
+++ components/page-title/index.vue
@@ -1,14 +1,24 @@
 <script setup lang="ts">
   defineProps<{
     title: string
+    subtitle?: string
   }>()
 </script>
 
 <template>
   <div class="mb-6 flex items-center justify-between">
-    <h1 class="text-3xl font-semibold tracking-tight text-neutral-900">
-      {{ title }}
-    </h1>
+    <div>
+      <h1 class="text-3xl font-semibold tracking-tight text-neutral-900">
+        {{ title }}
+      </h1>
+
+      <h2
+        class="tracking-none text-xl font-medium text-neutral-700"
+        v-if="subtitle"
+      >
+        {{ subtitle }}
+      </h2>
+    </div>
 
     <slot />
   </div>
--- components/page-title/page-title.test.ts
+++ components/page-title/page-title.test.ts
@@ -3,14 +3,19 @@ import { describe, expect, it } from 'vitest'
 import PageTitle from './index.vue'
 
 describe('PageTitle', () => {
-  const props = { title: 'Test Title' }
+  const props = {
+    title: 'Test Title',
+    subtitle: 'Test Subtitle',
+  }
 
   it('renders the title prop correctly', () => {
-    const wrapper = mount(PageTitle, {
-      props,
-    })
-
+    const wrapper = mount(PageTitle, { props })
     expect(wrapper.find('h1').text()).toBe(props.title)
+  })
+
+  it('renders the subtitle prop correctly', () => {
+    const wrapper = mount(PageTitle, { props })
+    expect(wrapper.find('h2').text()).toBe(props.subtitle)
   })
 
   it('renders default slot content', () => {
