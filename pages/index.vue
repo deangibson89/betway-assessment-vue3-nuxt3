ee8675246f4be65f1f4c8f128fad5bfaf772f0ea
Dean Gibson <dean.gibson@superbalist.com>
2024-10-08 08:46:16 +0200

    removed console logs

--- pages/index.vue
+++ pages/index.vue
@@ -13,9 +13,6 @@
 
   const fetchTabs = () => {
     const tabs = fetchAllTabs()
-    console.log({ tabs })
-
-    console.log(getTabTableDataFromTabs(tabs))
     data.value = getTabTableDataFromTabs(tabs)
   }
 
